4046b0812dd320bb4b3acc8fdbce76b9fd347c99
Kunal batham <89760031+KunalBatham2701@users.noreply.github.com>
2024-01-18 19:20:32 +0530

    Update NotFoundContact.jsx

--- src/components/NotFoundContact.jsx
+++ src/components/NotFoundContact.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const NotFoundContact = () => {
   return (
     <div className='flex justify-center items-center gap-4 h-[75vh]'>
-        <div><img src="/image/contact.png" alt="contact image" /></div>
+        <div><img src="https://png.pngtree.com/png-vector/20230414/ourmid/pngtree-phone-book-flat-icon-vector-png-image_6705450.png" className='size-20' alt="contact image" /></div>
       <h3 className='text-white text-2xl font-semibold'>No Contact Found</h3>
     </div>
   )
