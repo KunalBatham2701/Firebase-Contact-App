cd0c3abde4e73f773fac952b3c45fd838ad39309
Kunal Batham <kunal.batham_cs20@gla.ac.in>
2024-01-18 19:31:51 +0530

    second

--- src/components/Navbar1.jsx
+++ src/components/Navbar1.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const navbar = () => {
   return (
     <div className='h-[60px] bg-white rounded-lg flex justify-center gap-2 my-4 items-center text-xl font-medium'>
-        <img src="../../image/logos_firebase.svg" alt="image" />
+        <img src="https://cdn.iconscout.com/icon/free/png-256/firebase-3629364-3032357.png" className="size-12"alt="image" />
         <h1>My Contacts</h1>
     </div>
   )
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"https://KunalBatham2701.github.io/Firebase-Contact-App",
   plugins: [react()],
 })
